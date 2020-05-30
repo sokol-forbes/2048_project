49d4d184c503374ceed3b755848a7b037b3caf9c
unknown <sasha.sokolovsky01@gmail.com>
2020-05-30 16:23:57 +0300

    Some tests for functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 def pretty_print(mas):
     print('-' * 10)
     for row in mas:
@@ -7,6 +8,21 @@ def pretty_print(mas):
 
 def get_number_from_index(i, j):
     return i * 4 + j + 1
+
+
+def get_index_from_number(num):
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
+
+
+def insert_2_or_4(mas, x, y):
+    if random.random()<=0.75:
+     mas[x][y] = 2
+    else:
+        mas[x][y] = 4
+    return mas
+
 
 
 def get_empty_list(mas):
---
+++ test.py
@@ -0,0 +1,51 @@
+import unittest
+from main import get_number_from_index, get_empty_list, get_index_from_number
+
+
+class Test2048(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(get_number_from_index(1, 2), 7)
+
+    def test_2(self):
+        self.assertEqual(get_number_from_index(3, 3), 16)
+
+    def test_3(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+
+    def test_4(self):
+        a = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+
+    def test_5(self):
+        a = []
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+
+    def test_6(self):
+        self.assertEqual(get_index_from_number(7), (1, 2))
+
+    def test_7(self):
+        self.assertEqual(get_index_from_number(16), (3, 3))
+
+    def test_8(self):
+        self.assertEqual(get_index_from_number(1), (0, 0))
+
